6ddb36c2a92f9c4c3aab397778004445627048f9
Samuel Chanal <schanal@unistra.fr>
2024-09-18 17:21:43 +0200

    TP1 terminé

--- TP1/counter.php
+++ TP1/counter.php
@@ -4,19 +4,20 @@
 <html>
  
   <head>
-    <title>Exercice 3.1</title>
+    <title>Exercice 3.1 et 3.3</title>
   </head>
 
   <body>
     <?php if(!isset($_SESSION['nbr']))
     {
-        $_SESSION['nbr'] = 0;
+        $_SESSION['nbr'] = 1;
     }
     else {
         $_SESSION['nbr']++;
     }
-    
     echo "<p>Cette page a été visitée ".$_SESSION['nbr']." fois</p>";?>
+    
+    <a href="resetCounter.php">Remettre le compteur à 0</a>
   </body>
  
 </html>
